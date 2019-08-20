1c40ad6dc669fce3258ba938e27e77a4c87a6d5b
cardon <michel-cardon@laposte.net>
2019-08-20 12:22:00 +0200

    request

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
 class WelcomeController{
-
-    public function hello()
+    /**
+     * @Route("/hello", name="hello") 
+     */
+    public function hello(Request $request)
     {
         $name='mathieu';
-        return new Response('slt '.$name);
+        dump($request);
+        return new Response('<body>slt '.$name.'</body>');
+
     }
 }
